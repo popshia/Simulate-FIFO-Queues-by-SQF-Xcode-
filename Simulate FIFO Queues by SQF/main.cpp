15a69341291dd837bda05854ff4c1ba4bec266db
popshia <B123292179@gmail.com>
2018-11-21 13:30:38 +0800

    Initial Commit

---
+++ Simulate FIFO Queues by SQF/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Simulate FIFO Queues by SQF
+//
+//  Created by popshia on 2018/11/21.
+//  Copyright © 2018 popshia. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
